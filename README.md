cb73aab677ad305fe15259e3074bb9d27ee98431
dan3612812 <dan3612812@gmail.com>
2017-10-21 01:15:42 +0800

    this luhao image_server reform master image_server

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # image_server
+由mysql 限制
+ID clas cate
+
+ID為該資料表的用戶ID
+
+clas 為自己編的
+var pathtable = ["test", "mem", "shop", "bos", "pro", "gift", "ad"];
+
+cate 為該圖的種類 (目前只有mem_data 有兩種類的圖)
+var pathfield = ["test", "image", "logo", "cover"];
+
+所有種類
+ //所有正確組合 X11 X13 X22 X32 X41 X51 X61
+ 
+ ex1:當member id:1 想要上傳image
+ 傳{
+ "id":1,
+ "clas":1,
+ "cate":1,
+ "base64str":"image to base64"
+ }
+ ex2:當一個shop ID:10 想要上傳logo
+ 傳{
+ "id":10,
+ "clas":2,
+ "cate":2,
+ "base64str":"logo to base64"
+ }
+ 
+//後台該注意的
+由於 ad pro 都屬於次級資料表 屬於(shop or bos)
+但寫入還是要認ad_id or pro_id
+
+//管理帳號
+gift 寫入圖片也是任 gift_id
--- server.js
+++ server.js
@@ -5,15 +5,12 @@ var bodyParser = require('body-parser');
 var cookie = require('cookie-parser');
 var mysql = require('mysql');
 var auth = require('./auth.js');
-
 //var router = express.Router();
-
 //可以使用其他位置的物件
 //var routers = require('./routers');
 var fs = require('fs');
 //var upload = require('./fileupload.js');
-//----//
-
+const base64 = require('node-base64-image');
 
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
@@ -25,15 +22,8 @@ app.use(cookie());
 //handle request entity too large
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
-//---------
-
-//設置 session 可選參數
-//routers.setRequestUrl(app); //設定路徑
-
 //本機檔案 位子
 var path = ["test", "member", "shop", "boss", "product", "gift", "ad"];
-//----//
 //取得mysql 資訊
 var connection = mysql.createConnection({
     host: auth.MySQL.host,
@@ -43,7 +33,6 @@ var connection = mysql.createConnection({
     dataStrings: true
 });
 
-
 /* GET home page. */
 app.get('/', function (req, res) {
     console.log("index");
@@ -52,7 +41,7 @@ app.get('/', function (req, res) {
 });
 //路徑只能用' 不能用" 雙引號
 app.get('/upload', function (req, res) {
-    res.send('get /upload');
+    res.send('get /upload server is exist');
     console.log("get /upload");
     //res.render("upload");
 });
@@ -67,13 +56,8 @@ app.post('/upload', upload.single('avatar'), function (req, res, next) {
         console.log('上傳失敗');
         res.send('失敗');
     }
-
 });
 */
-//-------------------BASE64 -----------//
-
-const base64 = require('node-base64-image');
-
 //var base64str = base64_encode('./images/test/test.jpg');
 //var id = 0;
 //var clas = 0;
@@ -90,9 +74,9 @@ app.post('/createimage', function (req, res) {
             res.setHeader('status', 'success');
             res.json({ "createimage": 1 });
         } else {
-            console.log("createimage fail");
+            console.log("createimage fail because not change");
             res.setHeader('status', 'fail');
-            res.json({ "createimage": 1 });
+            res.json({ "createimage": 0 });
         }
 
     }
@@ -113,18 +97,20 @@ app.post('/getimage', function (req, res) {
     }
     getfile(id, clas, cate, temp);
 });
-function getfile(id, clas, cate, callback) {
 
+
+///////////function////////////
+function getfile(id, clas, cate, callback) {
     var tmpdir = './images/' + path[clas] + '/' + id + '/' + id + '-' + cate + '.txt';
     console.log('post getfile' + tmpdir);
     //檢查資料是否存在
     fs.stat(tmpdir, function (error, stat) {
-
         if (error == null) {
             //撈取資料
             console.log("the file is exist");
             fs.readFile(tmpdir, function (error, result) {
                 if (error) throw error;
+                //將檔案內容回傳
                 callback(result.toString());
             });
         } else {
@@ -133,10 +119,11 @@ function getfile(id, clas, cate, callback) {
         }
     });
 }
-
+//由post createfile呼叫  再呼叫writedatabase 寫進mysql
+//創建檔案 並寫檔案(image.txt)(base64)
 function createfile(id, clas, cate, base64str, callback) {
     //傳id 傳分類(mem,pro,shop,bos) 傳哪種圖片(大頭貼,封面照),傳base64
-
+    //檢查是否有file 不一定要做
     //創資料夾 
     createdir(path[clas], id);
     //C:\\Users\\winproluhao\\Desktop\\testimage\\t1\\imagesmember\\1\\1-1.txt
@@ -144,19 +131,27 @@ function createfile(id, clas, cate, base64str, callback) {
     //創檔 +寫檔
     fs.writeFile(tmpdir, base64str, function (err) {
         console.log(tmpdir);
-        if (err) return err;
+        if (err) throw err;
         console.log("write File");
         //寫進mysql
+        /*
+        var info = function (info){
+            callback(info);
+        };
+        */
         writedatabase(id, clas, cate);
         callback(1);
-
     });
 }
+//由 createfile 呼叫用於寫入mysql
 function writedatabase(id, clas, cate) {
+     //shop_logo mem_image mem_cover pro_image bos_logo gift_image ad_image
     var pathtable = ["test", "mem", "shop", "bos", "pro", "gift", "ad"];
-    //shop_logo mem_image mem_cover pro_image bos_logo gift_image ad_image
+    //mem 1, shop 2,bos 3, pro 4, gift 5,ad 6
     var pathfield = ["test", "image", "logo", "cover"];
-    //image 1 logo 2 cover 3
+    //image 1, logo 2, cover 3
+    //由於mysql 有些有欄位有些沒有 只有以下才有 X=id 當然 該資料表有該ID才會有東西
+    //所有正確組合 X11 X13 X22 X32 X41 X51 X61
     var table = pathtable[clas];
     var field = pathfield[cate];
     var tmpway = id + "/" + clas + "/" + cate;
@@ -164,37 +159,39 @@ function writedatabase(id, clas, cate) {
     var sql = "UPDATE `" + table + "_data` SET `" + table + "_" + field + "`='" + tmpway + "' WHERE `" + table + "_id` =" + id;
     //console.log(tmpway);
     console.log(sql);
-    /*
     connection.query(sql, function (error, result) {
         if (error) throw error;
+        /*
         if (result.changedRows == 0) {
             //修改失敗
-            console.log('///mysql Update is error///');
+            console.log('///mysql Update not change row///');
             callback(0);
         } else {
             //修改成功
             callback(1);
         }
+        */
     });
-    */
-
 }
-//將圖片轉base64 沒用 拿來測試的
-function base64_encode(file) {
-    var bitmap = fs.readFileSync(file);
-    return new Buffer(bitmap).toString('base64');
-}
+//由function createfile 呼叫用於創建dir
 //創建資料夾
 function createdir(clas, id) {
     var dir = './images/' + clas + '/' + id;
     console.log(dir);
     if (!fs.existsSync(dir)) {
         fs.mkdirSync(dir);
-        console.log("add");
+        console.log("add dir");
     }
 }
-//--------------------Base64 ----------//
 
+
+
+//--------------------Base64 ----------//
+//將圖片轉base64 沒用 拿來測試的
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file);
+    return new Buffer(bitmap).toString('base64');
+}
 
 http.listen(app.get('port'), '0.0.0.0', function () {
 
